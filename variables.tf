0ab008029035194fa04d825e4b1034f7f11c83b8
Prontenk <prontenkonazarij@gmail.com>
2024-08-28 17:41:20 +0300

    Видалені всі файли з гілки master

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "ami" {
-  description = "Amazon Machine Image ID"
-  default     = "ami-0c55b159cbfafe1f0"  # Замініть на актуальну AMI
-}
